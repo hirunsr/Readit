ff27973e801c289ab32d0cee64d8576ca778fce7
Hirun <hirun.sr@wonnapob.com>
2018-08-21 13:57:02 +0700

    first commit

---
+++ readItem.js
@@ -0,0 +1,43 @@
+// Modules
+const {BrowserWindow} = require('electron')
+
+// BrowserWindow
+let bgItemWin
+
+module.exports = (url, callback) => {
+
+	// Create new offscreen BrowserWindow
+  bgItemWin = new BrowserWindow({
+    width: 1000,
+    height: 1000,
+    show: false,
+    webPreferences: {
+      offscreen: true
+    }
+  })
+
+	// Load read item
+  bgItemWin.loadURL(url)
+
+	// Wait for page to finish loading
+  bgItemWin.webContents.on('did-finish-load', () => {
+
+    // Get screenshot (thumbnail)
+    bgItemWin.webContents.capturePage((image) => {
+
+      // Get image as dataURI
+      let screenshot = image.toDataURL()
+
+      // Get page title
+      let title = bgItemWin.getTitle()
+
+      // Return new item via callback
+      callback({ title, screenshot, url })
+
+      // Clean up
+      bgItemWin.close()
+      bgItemWin = null
+    })
+  })
+
+}
